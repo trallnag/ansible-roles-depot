a77754ed82c2cbe28ec27c3bd2a30e67bcdf83a9
Tim Schwenke <tim.schwenke@trallnag.com>
2022-09-14 15:09:40 +0200

    ssh: Add retry loop to agent trigger script

--- ssh/files/ssh-agent-trigger.bash
+++ ssh/files/ssh-agent-trigger.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 usage() {
-    cat <<EOF
+  cat << EOF
 Trigger the OpenSSH agent if default keyfile is not already cached.
 
 Assumes that the default key is "~/.ssh/id_ed25519".
@@ -16,13 +16,21 @@ ASKPASS="$HOME/.config/ssh/scripts/ssh-pass-passphrase.bash"
 KEYFILE="$HOME/.ssh/id_ed25519"
 
 if ! ssh-add -l | grep -q "$(ssh-keygen -l -f "$KEYFILE")"; then
-  printf '\033[1;34mOpenSSH:\033[0;34m Enter passphrase to unlock default \033[1;35mOpenSSH\033[0;34m private key...\033[0m\n'
+  rc=-1
+  while [ $rc != 0 ]; do
+    printf '\033[1;34mOpenSSH:\033[0;34m Enter passphrase to unlock default \033[1;35mSSH\033[0;34m private key\033[35;5m...\033[0m\n'
 
-  if read -s passphrase; then
-    if (( ${#passphrase} < 1 )); then
-      passphrase=x
+    if read -s passphrase; then
+      if ((${#passphrase} < 1)); then
+        passphrase=x
+      fi
+
+      SSH_ASKPASS_REQUIRE=force SSH_ASKPASS="$ASKPASS" ssh-add "$KEYFILE" <<< "$passphrase"
+      rc=$?
+
+      if [ $rc = 0 ]; then
+        printf "Unlocked default SSH private key and added it to ssh-agent.\n"
+      fi
     fi
-
-    SSH_ASKPASS_REQUIRE=force SSH_ASKPASS="$ASKPASS" ssh-add "$KEYFILE" <<< "$passphrase"
-  fi
+  done
 fi
--- ssh/templates/init-ssh-agent.bash
+++ ssh/templates/init-ssh-agent.bash
@@ -1,4 +1,4 @@
-source "{{ ssh_data_dir }}/agent.zsh" &> /dev/null
+source "{{ ssh_data_dir }}/agent.bash" &> /dev/null
 if ! ([[ -v SSH_AGENT_PID ]] && [[ -v SSH_AUTH_SOCK ]] && ps -q "$SSH_AGENT_PID" | grep "ssh-agent$" &> /dev/null); then
   rm -f "{{ ssh_data_dir }}/agent" &> /dev/null
   eval $(ssh-agent -a "{{ ssh_data_dir }}/agent") &> /dev/null
