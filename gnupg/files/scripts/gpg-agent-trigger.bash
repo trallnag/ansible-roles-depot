4eeb75f48e2bf94d6486f3751afeb854767c77f7
Tim Schwenke <tim.schwenke@trallnag.com>
2022-09-14 19:23:31 +0200

    gnupg: Add retry loop to agent trigger script

--- gnupg/files/scripts/gpg-agent-trigger.bash
+++ gnupg/files/scripts/gpg-agent-trigger.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 usage() {
-    cat <<EOF
+  cat << EOF
 Trigger the GnuPG agent if key / passphrase is not already cached.
 
 Keygrip is hardcoded into the script.
@@ -14,18 +14,24 @@ case $1 in help | -h | -help | --help) usage && exit ;; esac
 
 keygrip=AB71BEAEBFAEAC0B76F9C5D4E619CBF25F9A74F6
 
-if ! gpg-connect-agent 'keyinfo --list' /bye | grep -q "$keygrip D - - 1 P"; then
-  printf '\033[1;34mGnuPG:\033[0;34m Enter passphrase to unlock default \033[1;35mOpenPGP\033[0;34m secret key...\033[0m\n'
+if ! gpg-connect-agent "keyinfo --list" /bye | grep -q "$keygrip D - - 1 P"; then
+  rc=-1
+  while [ $rc != 0 ]; do
+    printf '\033[1;34mOpenPPG:\033[0;34m Enter passphrase to unlock default \033[1;35mPGP\033[0;34m secret key\033[35;5m...\033[0m\n'
 
-  if read -s pw; then
-    if (( ${#pw} < 1 )); then
-      pw=x
-    fi
+    if read -s passphrase; then
+      if ((${#passphrase} < 1)); then
+        passphrase=x
+      fi
 
-    # Passphrase is read from stdin. Phrase to sign is also read from stdin.
-    # Pinentry mode loopback ensures that pinentry is not used in any case.
-    if gpg --pinentry-mode=loopback --yes --sign --passphrase-fd 0 <<< "$pw" > /dev/null; then
-      echo "Unlocked default OpenPGP secret key and added it to gpg-agent."
+      # Passphrase is read from stdin. Phrase to sign is also read from stdin.
+      # Pinentry mode loopback ensures that pinentry is not used in any case.
+      gpg --pinentry-mode=loopback --yes --sign --passphrase-fd 0 <<< "$passphrase" &> /dev/null
+      rc=$?
+
+      if [ $rc = 0 ]; then
+        printf "Unlocked default PGP secret key and added it to gpg-agent.\n"
+      fi
     fi
-  fi
+  done
 fi
