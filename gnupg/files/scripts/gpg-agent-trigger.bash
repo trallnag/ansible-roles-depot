df3645fc4053217184dd863248d062cb727d1861
Tim Schwenke <tim.schwenke@trallnag.com>
2022-09-16 12:48:00 +0200

    gnupg: Add subshell and trap to gpg-agent-trigger

--- gnupg/files/scripts/gpg-agent-trigger.bash
+++ gnupg/files/scripts/gpg-agent-trigger.bash
@@ -14,24 +14,30 @@ case $1 in help | -h | -help | --help) usage && exit ;; esac
 
 keygrip=AB71BEAEBFAEAC0B76F9C5D4E619CBF25F9A74F6
 
+trap "" INT
+
 if ! gpg-connect-agent "keyinfo --list" /bye | grep -q "$keygrip D - - 1 P"; then
-  rc=-1
-  while [ $rc != 0 ]; do
-    printf '\033[1;34mOpenPPG:\033[0;34m Enter passphrase to unlock default \033[1;35mPGP\033[0;34m secret key\033[35;5m...\033[0m\n'
+  (
+    trap - INT
 
-    if read -s passphrase; then
-      if ((${#passphrase} < 1)); then
-        passphrase=x
+    rc=-1
+    while [ $rc != 0 ]; do
+      printf '\033[1;34mOpenPPG:\033[0;34m Enter passphrase to unlock default \033[1;35mPGP\033[0;34m secret key\033[35;5m...\033[0m\n'
+
+      if read -s passphrase; then
+        if ((${#passphrase} < 1)); then
+          exit
+        fi
+
+        # Passphrase is read from stdin. Phrase to sign is also read from stdin.
+        # Pinentry mode loopback ensures that pinentry is not used in any case.
+        gpg --pinentry-mode=loopback --yes --sign --passphrase-fd 0 <<< "$passphrase" &> /dev/null
+        rc=$?
+
+        if [ $rc = 0 ]; then
+          printf "Unlocked default PGP secret key and added it to gpg-agent.\n"
+        fi
       fi
-
-      # Passphrase is read from stdin. Phrase to sign is also read from stdin.
-      # Pinentry mode loopback ensures that pinentry is not used in any case.
-      gpg --pinentry-mode=loopback --yes --sign --passphrase-fd 0 <<< "$passphrase" &> /dev/null
-      rc=$?
-
-      if [ $rc = 0 ]; then
-        printf "Unlocked default PGP secret key and added it to gpg-agent.\n"
-      fi
-    fi
-  done
+    done
+  )
 fi
