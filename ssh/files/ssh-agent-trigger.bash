8eefa10cd7aa824ffc07ce6f2edc43c1bb5c8445
Tim Schwenke <tim.schwenke@trallnag.com>
2022-09-16 12:48:14 +0200

    ssh: Add subshell and trap to ssh-agent-trigger

--- ssh/files/ssh-agent-trigger.bash
+++ ssh/files/ssh-agent-trigger.bash
@@ -15,22 +15,28 @@ case $1 in help | -h | -help | --help) usage && exit ;; esac
 ASKPASS="$HOME/.config/ssh/scripts/ssh-pass-passphrase.bash"
 KEYFILE="$HOME/.ssh/id_ed25519"
 
+trap "" INT
+
 if ! ssh-add -l | grep -q "$(ssh-keygen -l -f "$KEYFILE")"; then
-  rc=-1
-  while [ $rc != 0 ]; do
-    printf '\033[1;34mOpenSSH:\033[0;34m Enter passphrase to unlock default \033[1;35mSSH\033[0;34m private key\033[35;5m...\033[0m\n'
+  (
+    trap - INT
 
-    if read -s passphrase; then
-      if ((${#passphrase} < 1)); then
-        passphrase=x
+    rc=-1
+    while [ $rc != 0 ]; do
+      printf '\033[1;34mOpenSSH:\033[0;34m Enter passphrase to unlock default \033[1;35mSSH\033[0;34m private key\033[35;5m...\033[0m\n'
+
+      if read -s passphrase; then
+        if ((${#passphrase} < 1)); then
+          exit
+        fi
+
+        SSH_ASKPASS_REQUIRE=force SSH_ASKPASS="$ASKPASS" ssh-add "$KEYFILE" <<< "$passphrase" &> /dev/null
+        rc=$?
+
+        if [ $rc = 0 ]; then
+          printf "Unlocked default SSH private key and added it to ssh-agent.\n"
+        fi
       fi
-
-      SSH_ASKPASS_REQUIRE=force SSH_ASKPASS="$ASKPASS" ssh-add "$KEYFILE" <<< "$passphrase"
-      rc=$?
-
-      if [ $rc = 0 ]; then
-        printf "Unlocked default SSH private key and added it to ssh-agent.\n"
-      fi
-    fi
-  done
+    done
+  )
 fi
