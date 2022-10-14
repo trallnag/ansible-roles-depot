d40498ddf6d71f48289b1ebd15da0e7c0ecc4905
Tim Schwenke <tim.schwenke@trallnag.com>
2022-10-14 20:31:23 +0200

    winsync: Add aws and add trigger

---
+++ winsync/templates/winsync-aws.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if ! command -v inotifywait > /dev/null; then
+  echo "inotifywait not installed." && exit 1
+fi
+
+if ! [ -d $HOME/.aws ]; then
+  echo "Directory not found: \$HOME/.aws" && exit 1
+fi
+
+if ! [ -d "{{ win_home }}/.aws" ]; then
+  echo "Directory not found: {{ win_home }}/.aws" && exit 1
+fi
+
+while inotifywait -q -q -e modify,move,create,delete $HOME/.aws; do
+  if [ -d $HOME/.aws ] && [ -d "{{ win_home }}/.aws" ]; then
+    rsync --ignore-missing-args -qavu \
+      $HOME/.aws/ \
+      "{{ win_home }}/.aws"
+  fi
+done
