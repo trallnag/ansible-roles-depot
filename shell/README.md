960ef79ba1c198dbe2bc8bb32c2bf4a0ae8ad78a
Tim Schwenke <tim@trallnag.com>
2022-10-31 23:23:13 +0100

    shell: Adjust fish notation

--- shell/README.md
+++ shell/README.md
@@ -2,3 +2,10 @@
 
 Contains shell commons for Bash, Zsh, and Fish. Only used from the respective
 roles for the aforementioned shells.
+
+The idea is that the three files in `files` contain a base configuration sourced
+by the individual shells.
+
+In addition, the individual shells add content to the three scripts from within
+their roles. The goal is that every shell has knowledge of other shells basic
+config.
--- shell/files/login.fish
+++ shell/files/login.fish
@@ -15,19 +15,19 @@ set x $HOME/.local/bin && contains $x $fish_user_paths && test -d $x && set -Ua 
 # -J or --status-column
 # -x4 or --tabs=4
 # -X or --no-init
-set -x LESS "-F -i -M -R -S -W -z-4 -J -x4"
+set -gx LESS "-F -i -M -R -S -W -z-4 -J -x4"
 
 # Language.
 if ! set -q LANG
-  set -x LANG en_US.UTF-8
+  set -gx LANG en_US.UTF-8
 end
 
 # Editors.
-set -x EDITOR nano
-set -x VISUAL nano
-set -x PAGER less
+set -gx EDITOR nano
+set -gx VISUAL nano
+set -gx PAGER less
 
 # This.
-set -x DOT_SHELL_LOGIN_BASH $HOME/.config/shell/login.bash
-set -x DOT_SHELL_LOGIN_FISH $HOME/.config/shell/login.fish
-set -x DOT_SHELL_LOGIN_ZSH $HOME/.config/shell/login.zsh
+set -gx DOT_SHELL_LOGIN_BASH $HOME/.config/shell/login.bash
+set -gx DOT_SHELL_LOGIN_FISH $HOME/.config/shell/login.fish
+set -gx DOT_SHELL_LOGIN_ZSH $HOME/.config/shell/login.zsh
