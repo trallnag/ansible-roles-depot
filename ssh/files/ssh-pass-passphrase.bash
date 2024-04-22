2553896f19d57d84bd14c6968e047f89407696ce
Tim Schwenke <tim@trallnag.com>
2024-04-22 23:34:19 +0200

    Delete role ssh

--- ssh/files/ssh-pass-passphrase.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Parameter $1 passed to the script is the prompt text.
-# READ Secret from STDIN and echo it.
-
-read passphrase
-echo "$passphrase"
--- ssh/templates/init-ssh-agent.zsh
+++
@@ -1,8 +0,0 @@
-source "{{ ssh_data_dir }}/agent.zsh" &> /dev/null
-if ! ([[ -v SSH_AGENT_PID ]] && [[ -v SSH_AUTH_SOCK ]] && ps -q "$SSH_AGENT_PID" | grep "ssh-agent$" &> /dev/null); then
-  rm -f "{{ ssh_data_dir }}/agent" &> /dev/null
-  eval $(ssh-agent -a "{{ ssh_data_dir }}/agent") &> /dev/null
-  echo "export SSH_AUTH_SOCK=$SSH_AUTH_SOCK; export SSH_AGENT_PID=$SSH_AGENT_PID" > "{{ ssh_data_dir }}/agent.zsh"
-  echo "export SSH_AUTH_SOCK=$SSH_AUTH_SOCK; export SSH_AGENT_PID=$SSH_AGENT_PID" > "{{ ssh_data_dir }}/agent.bash"
-  echo "set -x SSH_AUTH_SOCK $SSH_AUTH_SOCK; set -x SSH_AGENT_PID $SSH_AGENT_PID" > "{{ ssh_data_dir }}/agent.fish"
-fi
