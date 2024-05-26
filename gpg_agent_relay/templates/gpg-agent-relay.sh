2b8b352c681cf38f91d7ce3e02b968cea7060d86
Tim Schwenke <tim@trallnag.com>
2024-05-26 20:24:33 +0200

    gpg_agent_relay: Add removal of socket pre

--- gpg_agent_relay/templates/gpg-agent-relay.sh
+++ gpg_agent_relay/templates/gpg-agent-relay.sh
@@ -4,7 +4,7 @@ set -e
 
 export PATH="{{ gpg_agent_relay_win_ssh_exe }}:$PATH"
 
-ssh.exe -o ServerAliveInterval=60 -R /run/user/0/gnupg/S.gpg-agent:127.0.0.1:54321 root@localhost sh -s <<- 'EOF'
+ssh.exe -o ServerAliveInterval=60 -R /run/user/0/gnupg/S.gpg-agent:127.0.0.1:30782 root@localhost sh -s <<- 'EOF'
   set -e
   echo "Connection to localhost opened."
   gpg-connect-agent 'keyinfo --list' /bye
