bad721375379562abf03ec053d9d41f6684e3b9d
Tim Schwenke <tim@trallnag.com>
2024-08-09 12:10:43 +0200

    gnupg_agent_trigger: Add delete lock

--- gnupg_agent_trigger/templates/gnupg-agent-trigger.sh
+++ gnupg_agent_trigger/templates/gnupg-agent-trigger.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if [ -f "{{ gnupg_agent_trigger_lock }}" ]; then
+  echo "Found agent sentinel lock file."
+  rm -f "{{ gnupg_agent_trigger_lock }}"
+  echo "Deleted agent sentinel lock file."
+fi
+
 while true; do
   "{{ gnupg_agent_trigger_win_exe }}" /bye
   sleep 60
