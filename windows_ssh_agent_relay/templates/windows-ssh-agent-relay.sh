4c8c2bccae6f92784ecab2ba2b0128559733c83e
Tim Schwenke <tim@trallnag.com>
2024-11-10 12:49:00 +0100

    windows_ssh_agent_relay: Refactor and streamline with gpg

--- windows_ssh_agent_relay/templates/windows-ssh-agent-relay.sh
+++ windows_ssh_agent_relay/templates/windows-ssh-agent-relay.sh
@@ -2,18 +2,20 @@
 
 set -euo pipefail
 
-"{{ windows_ssh_agent_relay_ssh_exe }}" \
+echo "Connecting to target with SSH..."
+
+exec "{{ windows_ssh_agent_relay_ssh_exe }}" \
   -o ServerAliveInterval=60 \
-  {{ ansible_user }}@localhost \
+  "{{ ansible_user }}@localhost" \
   -A \
   sh -s \
   <<- 'EOF'
     set -e
 
-    echo "Connection opened."
+    echo "Connected to target with SSH."
 
     echo "Creating symlink..."
-    
+
     target="$SSH_AUTH_SOCK"
     link="{{ windows_ssh_agent_relay_socket }}"
     ln -sf "$target" "$link"
