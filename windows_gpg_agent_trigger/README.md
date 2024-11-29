d104bcd707ae0d2e5cfdfd1cb19214ef5c20ed28
Tim Schwenke <tim@trallnag.com>
2024-11-29 12:04:11 +0100

    windows_gpg_agent_trigger: Streamline

--- windows_gpg_agent_trigger/README.md
+++ windows_gpg_agent_trigger/README.md
@@ -1,3 +1,3 @@
 # Ansible Role `windows_gpg_agent_trigger`
 
-Sets up a systemd service that regularly triggers the GPG agent in Windows.
+Sets up a systemd service that regularly triggers GPG Agent in Windows.
--- windows_gpg_agent_trigger/templates/windows-gpg-agent-trigger.sh
+++ windows_gpg_agent_trigger/templates/windows-gpg-agent-trigger.sh
@@ -2,12 +2,12 @@
 
 set -euo pipefail
 
-if [[ -f "{{ windows_gpg_agent_trigger_lock }}" ]]; then
-  rm -f "{{ windows_gpg_agent_trigger_lock }}"
+if [[ -f "{{ windows_gpg_agent_trigger_lock_path }}" ]]; then
+  rm -f "{{ windows_gpg_agent_trigger_lock_path }}"
   echo "Deleted agent sentinel lock file."
 fi
 
 while true; do
-  "{{ windows_gpg_agent_trigger_exe }}" --verbose /bye
+  "{{ windows_gpg_agent_trigger_exe_path }}" --verbose /bye
   sleep 60
 done
