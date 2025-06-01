f63feeae17a20bcd40b53f884e2a583e3391a4ee
Tim Schwenke <tim@trallnag.com>
2025-06-01 12:57:27 +0200

    windows_powershell: Update style

--- windows_powershell/files/trust-repo-psgallery.ps1
+++
@@ -1,6 +0,0 @@
-Set-StrictMode -Version Latest
-
-if ((Get-PSRepository -Name PSGallery).InstallationPolicy -ne "Trusted") {
-  Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-  Write-Output "status=changed"
-}
