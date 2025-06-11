180a44ab9d58cf9651da2d461ab4e1389ca6906d
Tim Schwenke <tim@trallnag.com>
2025-06-11 23:08:26 +0200

    windows_gpg_agent_relay: Refactor

--- windows_gpg_agent_relay/templates/windows-gpg-agent-relay.sh
+++ windows_gpg_agent_relay/templates/windows-gpg-agent-relay.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/bash
+#!/usr/bin/sh
 
-set -euo pipefail
+set -eu
 
 r_remote="/run/user/{{ ansible_facts.user_uid }}/gnupg/S.gpg-agent"
 r_local="{{ windows_gpg_bridge_extra_host }}:{{ windows_gpg_bridge_extra_port }}"
 
-echo "Connecting to target with SSH..."
+echo "Connecting to target with OpenSSH..." >&2
 
 exec "{{ windows_gpg_agent_relay_ssh_exe_path }}" \
   -o ServerAliveInterval=60 \
@@ -15,14 +15,14 @@ exec "{{ windows_gpg_agent_relay_ssh_exe_path }}" \
   <<- 'EOF'
     set -e
 
-    echo "Connected to target with SSH."
+    echo "Connected to target with OpenSSH." >&2
 
-    echo "Pinging GPG agent..."
+    echo "Pinging GnuPG agent..." >&2
 
     gpg-connect-agent "keyinfo --list" /bye
 
-    echo "Pinged GPG agent."
+    echo "Pinged GnuPG agent." >&2
 
-    echo "Sleeping forever..."
+    echo "Sleeping forever..." >&2
     sleep infinity
 EOF
