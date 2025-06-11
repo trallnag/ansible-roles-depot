b104e5e2bd83b5b65cbb38fa6765e85b2cb61d60
Tim Schwenke <tim@trallnag.com>
2025-06-11 23:45:32 +0200

    homebrew: Streamline

--- homebrew/templates/install.sh
+++ homebrew/templates/install.sh
@@ -10,7 +10,7 @@ if ! test -d "$homebrew_exe_dir_path"; then
   # Check if sudo can be run without a password.
   if ! sudo -n true 2> /dev/null; then
     # Provide the sudo password if needed.
-    printf %s "$ansible_become_pass" | sudo -S whoami &> /dev/null
+    printf '%s' "$ansible_become_pass" | sudo -S whoami &> /dev/null
   fi
 
   # Verify sudo access again.
@@ -23,7 +23,7 @@ if ! test -d "$homebrew_exe_dir_path"; then
     bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
     # Indicate that the status has changed.
-    echo status=changed
+    echo "status=changed"
   else
     # Print an error message if sudo is not working.
     echo "Sudo not working."
