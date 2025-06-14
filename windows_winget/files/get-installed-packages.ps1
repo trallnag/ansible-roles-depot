e1382df5de6c395ecc4685a5bab35de583829ffb
Tim Schwenke <tim@trallnag.com>
2025-06-14 14:57:55 +0200

    windows_winget: Split up and improve

--- windows_winget/files/get-installed-packages.ps1
+++ windows_winget/files/get-installed-packages.ps1
@@ -1,3 +1,7 @@
+$Env:__SuppressAnsiEscapeSequences = "1"
+$Env:NO_COLOR = "1"
+$Env:TERM = "dumb"
+$ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
 Get-WinGetPackage `
--- windows_winget/files/install-module-client.ps1
+++ windows_winget/files/install-module-client.ps1
@@ -1,10 +1,27 @@
+$Env:__SuppressAnsiEscapeSequences = "1"
+$Env:NO_COLOR = "1"
+$Env:TERM = "dumb"
+$ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
-$moduleName = "Microsoft.WinGet.Client"
+function Install-ModuleWrapped {
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory = $true)]
+    [string]$ModuleName
+  )
 
-$module = Get-InstalledModule -Name $moduleName -ErrorAction SilentlyContinue
+  $module = Get-InstalledModule -Name $ModuleName -ErrorAction SilentlyContinue
 
-if ($module -eq $null) {
-  Install-Module -Name $moduleName -Scope CurrentUser
-  Write-Output "status=changed"
+  if ($module -eq $null) {
+    Install-Module `
+      -Name $ModuleName `
+      -Repository PSGallery `
+      -Scope CurrentUser `
+      -AllowClobber
+
+    Write-Output "status=changed"
+  }
 }
+
+Install-ModuleWrapped -ModuleName Microsoft.WinGet.Client
