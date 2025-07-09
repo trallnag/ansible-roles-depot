c868bbfd7adc98d326898c91fa903c80a6c48e2c
Tim Schwenke <tim@trallnag.com>
2025-07-09 23:25:53 +0200

    windows_ssh_agent_relay: Streamline

--- windows_ssh_agent_relay/README.md
+++ windows_ssh_agent_relay/README.md
@@ -1,3 +1,9 @@
 # Ansible role `windows_ssh_agent_relay`
 
 Sets up a systemd service that forwards the OpenSSH agent from Windows to Linux.
+
+## Maintenance
+
+### Update
+
+Nothing to do.
--- windows_ssh_agent_relay/templates/windows-ssh-agent-relay.sh
+++ windows_ssh_agent_relay/templates/windows-ssh-agent-relay.sh
@@ -3,22 +3,22 @@
 set -eu
 
 # Create socket directory if it does not exist yet.
-if ! [ -d "{{ windows_ssh_agent_relay_socket_path | dirname }}" ]; then
+if ! [ -d "{{ windows_ssh_agent_relay__socket_path | dirname }}" ]; then
   echo "Creating socket directory..." >&2
-  mkdir "{{ windows_ssh_agent_relay_socket_path | dirname }}"
+  mkdir "{{ windows_ssh_agent_relay__socket_path | dirname }}"
   echo "Created socket directory." >&2
 fi
 
 # Remove socket if it already exists.
-if [ -z "{{ windows_ssh_agent_relay_socket_path }}" ]; then
+if [ -f "{{ windows_ssh_agent_relay__socket_path }}" ]; then
   echo "Removing socket..." >&2
-  rm "{{ windows_ssh_agent_relay_socket_path }}"
+  rm "{{ windows_ssh_agent_relay__socket_path }}"
   echo "Removed socket." >&2
 fi
 
 echo "Connecting to target with OpenSSH..." >&2
 
-exec "{{ windows_ssh_agent_relay_ssh_exe_path }}" \
+exec "{{ windows_ssh_agent_relay__ssh_exe_path }}" \
   -o ServerAliveInterval=60 \
   "{{ ansible_user }}@localhost" \
   -A \
@@ -31,7 +31,7 @@ exec "{{ windows_ssh_agent_relay_ssh_exe_path }}" \
     echo "Creating symlink..." >&2
 
     target="$SSH_AUTH_SOCK"
-    link="{{ windows_ssh_agent_relay_socket_path }}"
+    link="{{ windows_ssh_agent_relay__socket_path }}"
     ln -sf "$target" "$link"
 
     echo "Created symlink. target='$target' link='$link'" >&2
